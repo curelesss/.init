fa66e4621b2345077ff04187aad65b17a01db50a
curelesss <curelesss@gmail.com>
2024-10-10 11:16:17 +0800

    minor

--- termux.init.sh
+++ termux.init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+mv termux/termux.init.sh ./
+
 termux-setup-storage
+sleep 30s
 termux-change-repo
 
 pkg update
@@ -13,3 +16,10 @@ export CARGO_BUILD_TARGET=aarch64-linux-android
 apt install python-cryptography
 pip install ansible
 
+ansible-playbook playbook.yml --ask-vault-pass --tags=github
+
+git remote set-url origin git@github.com:curelesss/.init.git
+
+ssh -T git@github.com
+
+mv termux.init.sh termux/
