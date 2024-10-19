825c3939f4f890c7a831abd757bc35ac4f1a3124
curelesss <curelesss@gmail.com>
2024-10-19 16:39:33 +0800

    ubuntu init scprit

--- 1.github.ubuntu.sh
+++ 1.github.ubuntu.sh
@@ -1,8 +1,43 @@
+echo -*********************************-
+echo -** Update Ubuntu Mirror List   **-
+echo -*********************************-
+
+sudo rm -f /etc/apt/sources.list
+sudo cp ./sources.list /etc/apt/
+
+echo -*********************************-
+echo -** Add Ansible Repository      **-
+echo -*********************************-
+
 sudo apt-add-repository ppa:ansible/ansible -y
+
+echo -*********************************-
+echo -** Update and Upgrade Sysytem  **-
+echo -*********************************-
+
 sudo apt update
+sudo apt upgrade -y
+
+echo -*********************************-
+echo -** Install Ansible             **-
+echo -*********************************-
+
 sudo apt install software-properties-common ansible -y
 
+echo -*********************************-
+echo -** Set current repo to ssh     **-
+echo -*********************************-
 
 git remote set-url origin git@github.com:curelesss/init.git
-ansible-playbook playbook.yml --become-password-file=sudo --ask-vault-pass --tags=github
+
+echo -*********************************-
+echo -** Run Ansible Init Play-book  **-
+echo -*********************************-
+
+ansible-playbook playbook.yml --ask-vault-pass
+
+echo -*********************************-
+echo -** Testing Github Connection   **-
+echo -*********************************-
+
 ssh -T git@github.com
