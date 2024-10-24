8ad90634d5a897fb9623aa66d3e517ac7f362ad1
curelesss <curelesss@gmail.com>
2024-10-24 21:18:06 +0800

    minor

--- scoop.install.ps1
+++ scoop.install.ps1
@@ -18,3 +18,11 @@ else {
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 
+# Install chocolatey
+if ([bool](Get-Command -Name 'choco' -ErrorAction SilentlyContinue)) {
+    Write-Verbose "Chocolatey is already installed, skip installation." -Verbose
+}
+else {
+    Write-Verbose "Installing Chocolatey..." -Verbose
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
