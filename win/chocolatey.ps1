16314a87634c45e49d6e27584404c0f6008128c7
curelesss <curelesss@gmail.com>
2024-10-27 01:12:02 +0800

    minor

--- win/chocolatey.ps1
+++ win/chocolatey.ps1
@@ -8,15 +8,15 @@ else {
     Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 }
 
-# Install Scoop
-if (Get-Command -Name 'scoop' -ErrorAction SilentlyContinue) {
-    Write-Verbose "Scoop is already installed, skip installation." -Verbose
-}
-else {
-    Write-Verbose "Installing Scoop..." -Verbose
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-}
+# # Install Scoop
+# if (Get-Command -Name 'scoop' -ErrorAction SilentlyContinue) {
+#     Write-Verbose "Scoop is already installed, skip installation." -Verbose
+# }
+# else {
+#     Write-Verbose "Installing Scoop..." -Verbose
+#     Set-ExecutionPolicy Bypass -Scope Process -Force
+#     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+# }
 
 # Install chocolatey
 if ([bool](Get-Command -Name 'choco' -ErrorAction SilentlyContinue)) {
